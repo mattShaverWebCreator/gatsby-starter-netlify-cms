6129be67d2033cce41866b88d526ad3b1c61d26d
mattShaverWebCreator <mattshaver@boostthegram.com>
2019-04-14 12:44:44 -0400

    Updated Navbar: Removed Form Examples link, Updated Navbar to include projects and added LinkedIn, Instagram SVG icon.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
+import linkedin from "../img/linkedin.svg";
+import instagram from "../img//social/instagram.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
@@ -64,16 +66,13 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Projects
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
@@ -85,6 +84,12 @@ const Navbar = class extends React.Component {
               >
                 <span className="icon">
                   <img src={github} alt="Github" />
+                </span>
+                <span className="icon">
+                  <img src={instagram} alt="Instagram" />
+                </span>
+                <span className="icon">
+                  <img src={linkedin} alt="LinkedIn" />
                 </span>
               </a>
             </div>
